004c88268f639fcdbb0d7e8ed18ce3887b5a44a4
Leo <cacate0129@gmail.com>
2017-12-05 17:36:06 +0800

    fix a bug

--- nrg/impurity.py
+++ nrg/impurity.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-from tba.hgen import SuperSpaceConfig, sz, sx, sy
-from tba.hgen import op_U, Operator
-
-
 def anderson_impurity(U, mu, Bz=0.):
     h = np.diag([0, -mu+Bz, -mu-Bz, U-2*mu])
     return h
